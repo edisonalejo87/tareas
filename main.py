f6b87b0aa9539ce47847be7a571c5e780ae77a2c
Edison Guachan <edison.guachan@educacion.gob.ec>
2023-01-14 14:38:45 -0500

    strings

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-nombre = (input("Ingrese el Nombre "))
-apellido = (input("Ingrese el apellido "))
-edad = (input("Ingrese la edad "))
-fullname="Mi nombre es " + nombre + apellido + " tengo una edad de " + edad
-print(fullname)
+#nombre = (input("Ingrese el Nombre "))
+#apellido = (input("Ingrese el apellido "))
+#edad = (input("Ingrese la edad "))
+#fullname="Mi nombre es " + nombre + apellido + " tengo una edad de " + edad
+#Eprint(fullname)
+
+# name1="Edison"
+# text = "Hola como estas  "
+# nombre = input(" Ingrese su nombre ")
+# print (name1 == nombre)
+# #imprimir texto concatenando
+# fultext = text + nombre
+# #imprimir texto concatenando
+# fultext1 = f"{text} {nombre}...!"
+# print (fultext)
+# print (fultext1)
+# print(name1.upper())
+
+
+# import texto.reordenar as reordenar
+# while True:
+#     texto=input("Humano por favor ingresa un palindromo:")
+#     esPalindromo=reordenar.palindromo(texto)
+#     if esPalindromo==True:
+#         print(f"Felicidades humano '{texto}' si es palindromo")
+#         break
+#     else:
+#         print(f"Humano tonto '{texto}' no es palindromo")
+
+
+palabra = (input("Ingrese una palabra"))
+palabra1 = palabra[::-1]
+print(palabra[::-1])
+print (palabra == palabra1 ) 
+
+
