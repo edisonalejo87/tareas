738e2d1fe481d9902d4b5f3ec2c96f405ee7eee5
Edison Alejandro Guachán Iñiguez <edison.guachan@educacion.gob.ec>
2023-01-14 12:27:00 -0500

    Archivo de ejemplo para subir a git

---
+++ main.py
@@ -0,0 +1,6 @@
+nombre = (input("Ingrese el Nombre "))
+apellido = (input("Ingrese el apellido "))
+edad = (input("Ingrese la edad "))
+
+fullname="Mi nombre es " + nombre + apellido + " tengo una edad de " + edad
+print(fullname)
