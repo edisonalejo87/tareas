988d3b8bee6bbe614ba60251e58ff3ea06e16165
Edison Guachan <edison.guachan@educacion.gob.ec>
2023-01-14 14:44:34 -0500

    Estructura de Carpetas

---
+++ Codigos/Semana1/index.py
@@ -0,0 +1,70 @@
+"""print("Edison")
+print(2)
+print(True)
+print(7.2)
+print(type("Edison"))
+print(type(2))
+print(type(True))
+print(type(7.2))
+
+var_name = "Edison"
+print(var_name)
+var_num1 = 3
+print(var_num1)
+
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+
+var_num2 = 3
+var_num3 = 2
+
+print(var_num2+var_num3)
+print(var_num2-var_num3)
+print(var_num2*var_num3)
+print(var_num2/var_num3)
+print(var_num2**var_num3)
+
+print("El resultado es" ,var_num2+var_num3)
+print("El resultado es" ,var_num2-var_num3)
+print("El resultado es" ,var_num2*var_num3)
+print("El resultado es" ,var_num2/var_num3)
+print("El resultado es" ,var_num2**var_num3)
+
+var_num4 = int(input("Ingrese el primer número "))
+print(var_num4)
+
+var_num5 = int(input("Ingrese el segundo número "))
+print(var_num5)
+
+print("El resultado de la suma es" ,var_num4+var_num5)
+print("El resultado de la resta es" ,var_num4-var_num5)
+print("El resultado de la multiplicacion es" ,var_num4*var_num5)
+print("El resultado de la división es" ,var_num4/var_num5)"""
+
+
+num1=7
+num2=3
+num3 = int(input("Ingrese el número "))
+if num3>=num1:
+  print('El  número ingresado es mayor que 7.')
+elif num3<=num1:
+  print('El  número ingresado es menor que 7.')
+else:
+  print('Es el mismo número que el ingresado.')
+
+"""
+num1=7
+num2=3
+num3 = int(input("Ingrese el número "))
+
+num4= num1 >= num3
+
+print("El numero" ,num1 ,"es mayor que el ingresado",num3, num1 >= num3)
+print("El numero" ,num1 ,"es menor que el ingresado",num3, num1 <= num3)"""
+
+
+
+
+
